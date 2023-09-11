299c325e1483d61dfaab1da2d071d76d7ef4c03d
TheTuTran <131922588+TheTuTran@users.noreply.github.com>
2023-09-11 08:22:49 -0500

    first commit

---
+++ src/components/RouletteButton.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface RouletteButtonProps {}
+
+const RouletteButton: FC<RouletteButtonProps> = ({}) => {
+  return <section>RouletteButton</section>;
+};
+
+export default RouletteButton;
